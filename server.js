1fbe21817cc22bbd06b04fb5b1f61c7334407d7a
Israel Adetunji <adisen230@gmail.com>
2019-12-14 01:31:01 +0100

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /** @format */
 
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
 
 const connectDB = require('./config/db');
 
@@ -11,14 +11,20 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors());
 app.use(express.json({ extended: false }));
 
 // Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
-app.get('/', (req, res) => res.send('api running'));
+// Serve stativ assests in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
